11fe78bfa6dafda1f9286950b2457ad768729f2c
Sam Noyes <Clam99@users.noreply.github.com>
2015-03-11 17:32:12 -0600

    Added title, play button, and high score label and edited plist to lock in landscape

--- CrazyBalls/MainMenu.swift
+++ CrazyBalls/MainMenu.swift
@@ -1,20 +1,33 @@
 import Foundation
 import UIKit
 
-class MainMenu: UIView
+class MainMenu: UIViewController
     var balls:[Ball] = []
     var timerCount = 0
+    let timer:NSTimer!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var playBut: UIButton!
+    @IBOutlet weak var highscoreText: UILabel!
+    
     override init() {
-        NSTimer.scheduledTimerWithTimeInterval(0.01, target: self, selector: "timerFunc" repeats: true)
+        let timer = NSTimer.scheduledTimerWithTimeInterval(0.01, target: self, selector: "timerFunc" repeats: true)
+        title.alpha = 0.0
+        playBut.alpha = 0.0
+        highscoreText.alpha = 0.0
     }
     func timerFunc() {
         timerCount++
-        if (timerCount >= 10) {
-            timerCount = 0
+        if (timerCount >= 2) {
             balls.append(Ball(x:0, y:0, radius:50,radiansCounterClockwiseFromHorizontal:arc4random()%(30*(M_PI/180)), initialVelocity: 10))// - append balls facing in random directions
             balls.append(Ball(x:0, y:0, radius:50,radiansCounterClockwiseFromHorizontal:arc4random()%(30*(M_PI/180))+30, initialVelocity: 10))// - append balls facing in random directions
             balls.append(Ball(x:0, y:0, radius:50,radiansCounterClockwiseFromHorizontal:arc4random()%(30*(M_PI/180))+60, initialVelocity: 10))// - append balls facing in random directions
         }
+        if (timerCount>=3000) {
+            timer.invalidate()
+            timer = nil
+            showTitle()
+        }
+        
         setNeedsDisplay()
     }
     override func drawRect(rect:CGRect) {
@@ -24,5 +37,17 @@ class MainMenu: UIView
             UIColor.blackColor().setFill()
             circle.fill()
         }
+    }
+    func showTitle() {
+        UIView.animateWithDuration(1.5, animations: {
+            label.alpha = 1.0
+        })
+        showMenu()
+    }
+    func showMenu() {
+        UIView.animateWithDuration(1.0, animations: {
+            playButton.alpha = 1.0
+            highscoreText.alpha = 1.0
+        })
     }
 }
